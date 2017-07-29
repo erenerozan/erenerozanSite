ca188f59b95bbd3d887303516bbeb30e170f23cc
Eren EROZAN <erenerozan@gmail.com>
2017-07-29 16:46:01 +0300

    düzenleme ve scss eklemesi

---
+++ erenerozan/src/title.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Title extends Component {
+  render() {
+    return (
+      "deneme"
+    );
+  }
+}
+
+export default Title;
